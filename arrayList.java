11dabf12fc60dc18bbe85df952f525d7fa60c22f
raunakmishra18 <mraunak255@gmail.com>
2025-06-20 10:53:40 +0530

    oush

---
+++ arrayList.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> List=new ArrayList<>(10);
+        List.add(10);
+        List.add(20);
+        List.add(30);
+        List.add(40);
+        List.add(50);
+        List.add(60);
+        List.add(70);
+        List.add(80);
+        List.add(90);
+        List.add(100);
+        List.add(110);
+        List.add(120);
+        List.add(130);
+        List.add(140);
+        List.add(150);
+        List.add(160);
+        List.add(170);
+        List.add(180);
+        List.add(190);
+        List.add(200);
+        List.add(210);
+        System.out.println(List);
+        System.out.println(List.size()); // Size of the ArrayList
+
+    }
+}
